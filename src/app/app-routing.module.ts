da910a7998894c98346650eb80aa88a6bbbbd43c
pranav8121 <71267129+pranav8121@users.noreply.github.com>
2021-10-25 23:06:42 +0530

    Folder Structure Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Component/login/login.component';
-import { MainComponent } from './Component/main/main.component';
+import { MainComponent } from './Component/Dairy Reg/main/main.component';
 
 const routes: Routes = [
   {path:'', component:LoginComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,16 +10,16 @@ import { OrderModule } from 'ngx-order-pipe';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Component/login/login.component';
-import { MainComponent } from './Component/main/main.component';
+import { MainComponent } from './Component/Dairy Reg/main/main.component';
 import { HeaderComponent } from './Component/header/header.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DetailComponent } from './Component/detail/detail.component';
+import { DetailComponent } from './Component/Dairy Reg/detail/detail.component';
 import { TrimdatePipe } from './Pipes/trimdate.pipe';
 import { NgxPrintModule } from 'ngx-print';
-import { LastComponent } from './Component/last/last.component';
-import { AdvanceComponent } from './Component/advance/advance.component';
-import { SupplyComponent } from './Component/supply/supply.component';
+import { LastComponent } from './Component/Dairy Reg/last/last.component';
+import { AdvanceComponent } from './Component/Dairy Reg/advance/advance.component';
+import { SupplyComponent } from './Component/Dairy Reg/supply/supply.component';
 
 @NgModule({
   declarations: [
