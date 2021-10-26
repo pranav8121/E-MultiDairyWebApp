4e3c3a9b790defd93d8f31e8a9ea48d363154f34
pranav8121 <71267129+pranav8121@users.noreply.github.com>
2021-10-26 11:45:42 +0530

    errr

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Component/Xparts/login/login.component';
-import { MainComponent } from './Component/Member Reg/main/main.component';
-import { PaymentComponent } from './Component/Payment Reg/payment/payment.component';
-import { AddmemComponent } from './Component/Member Reg/addmem/addmem.component';
+import { MainComponent } from './Component/MemberReg/main/main.component';
+import { PaymentComponent } from './Component/PaymentReg/payment/payment.component';
 
 const routes: Routes = [
   {path:'', component:LoginComponent},
   {path:'Main', component:MainComponent},
   {path:'Payment', component:PaymentComponent},
-  {path:'AddMember', component:AddmemComponent},
 ];
 
 @NgModule({
