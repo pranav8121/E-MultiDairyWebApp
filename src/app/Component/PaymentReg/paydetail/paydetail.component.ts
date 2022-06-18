14d9f84c306051a4ca29582029fad09799656afc
ashokrpatil <ashokrpatil_engg@yahoo.co.in>
2022-06-19 00:37:14 +0530

    UI updated for Colors

--- src/app/Component/PaymentReg/paydetail/paydetail.component.ts
+++ src/app/Component/PaymentReg/paydetail/paydetail.component.ts
@@ -2,6 +2,9 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from 'src/app/Service/api.service';
 import * as xlsx from 'xlsx';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from "pdfmake/build/vfs_fonts";
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-paydetail',
@@ -18,9 +21,11 @@ to:any
   hideErr:any
   mainData:any
   @ViewChild('tabledata', { static: false }) tabledata: any;
+  dairyName: any;
   constructor(private _Activatedroute:ActivatedRoute,private _api:ApiService,private router:Router) { }
   ngOnInit(): void {
-    this.getData()    
+    this.getData();
+    this.dairyName=sessionStorage.getItem('Name') 
   }
   getData(){
     let f:any=this._Activatedroute.snapshot.paramMap.get("from")
@@ -55,6 +60,64 @@ to:any
 
   goBack(){
     this.router.navigate([`/Payment`]);
+  }
+  generatePDF(action = 'open') {
+    
+    let docDefinition:any = {
+      content: [
+
+                 {
+            text:this.dairyName,
+            fontSize: 12,
+            alignment: 'center',
+            color: 'brown'
+          },
+        {
+          table: {
+            headerRows: 1,
+            widths: ['auto','auto', '*','*','*', 'auto', 'auto'],
+            body: [
+              [{text: 'SN', bold: true},{text: 'Name', bold: true},{text: 'Mor', bold: true}, {text: 'Eve', bold: true},{text: 'Milk', bold: true},{text: 'Rate', bold: true},{text: 'Advace', bold: true}],
+               ...this.data.map((p:any) => ([p.No,p.Name, p.totalmilk, p.mortotalRate,p.evetotalRate,p.totalRate,p.adv])),
+              // [{ text: '', colSpan: 4 },{}, {}, {},{text: 'Amount',colSpan: 2},{}, this.base_amount],
+              // [{ text: ``, colSpan: 4 },{}, {}, {},{text: `CGST ${this.invoice.cgst} %`,colSpan: 2},{}, this.cgst_amount],
+             
+            ]
+           
+          }
+        },
+       
+      
+      ],
+      styles: {
+        sectionHeader: {
+          bold: true,
+          decoration: 'underline',
+          fontSize: 12,
+          margin: [0, 10, 0, 10]
+        },
+        space: {
+          // bold: true,
+          // decoration: 'underline',
+          // fontSize: 14,
+          margin: [0, 10, 0, 10]
+        }
+      },
+      
+    };
+
+
+
+    if (action === 'download') {
+     
+      pdfMake.createPdf(docDefinition).download();
+    } 
+    // else if (action === 'print') {
+    //   pdfMake.createPdf(docDefinition).print();
+    // } else {
+    //   pdfMake.createPdf(docDefinition).open();
+    // }
+
   }
 
 }
